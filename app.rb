c0661626eb61257306279d48cde80bd90760131d
txderosa <tderosa3@binghamton.edu>
2016-05-03 20:26:48 +0000

    Fixed app.rb

--- app.rb
+++ app.rb
@@ -39,7 +39,15 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+      if @game.wrong_guesses.include? letter or @game.guesses.include? letter
+        flash[:message] = "You have already used that letter."
+      else
+        @game.guess(letter)
+      end
+    rescue
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,16 +58,20 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    redirect '/win' if @game.check_win_or_lose == :win
+    redirect '/lose' if @game.check_win_or_lose == :lose
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' if @game.check_win_or_lose != :win 
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' if @game.check_win_or_lose != :lose
     erb :lose # You may change/remove this line
   end
   
